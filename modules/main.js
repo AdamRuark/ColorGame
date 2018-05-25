68a734f5beb79e4f01c47392555a0693686487c3
AdamRuark <adamruark15@gmail.com>
2018-05-24 23:05:17 -0700

    Page reloads when no levels left

--- modules/main.js
+++ modules/main.js
@@ -2,12 +2,17 @@ var Game = require('./game.js');
 var LevelGenerator = require('./levelGenerator');
 
 var curLevel = 1;
-var numLevels = 2;
 
 Game.init();
 async function levelLoop() {
 	var response = await fetch('/level/' + curLevel);
+
 	var data = await response.json();
+
+	// No possible level
+	if(Object.keys(data).length == 0) {
+		window.open('index.html', '_self');
+	}
 	var level = LevelGenerator.generate(data);
 
 	Game.newLevel(level);
@@ -16,9 +21,7 @@ async function levelLoop() {
 		if(Game.levelEnd) {
 			clearInterval(interval);
 			curLevel++;
-			if(curLevel <= numLevels){
-				levelLoop();
-			}
+			levelLoop();
 		}
 	}, 16.67);
 }
--- server.js
+++ server.js
@@ -4,32 +4,30 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var package = require('./compress.js');
-var papa = require('papaparse')
+var papa = require('papaparse');
+
+var numLevels = fs.readdirSync(path.join(__dirname, '/levels/')).length - 1;
 
 app.use(express.static('public'));
 
 app.get('/level/:num', function(req, res) {
-	console.log('get request');
-	var level = path.join(__dirname, '/levels/level' + req.params.num + '.csv');
-	var stream = fs.createReadStream(level);
-	stream.on('open', function() {
-		papa.parse(stream, {
-			complete: function(results, file) {
-
-				/*if(results.data.length != 100 && results.data[0].length != 100) {
-					// Send default value (change to something else later)
-					res.send(null);
-				}
-				else {
+	console.log('Get level:' + req.params.num);
+	if(req.params.num > numLevels) {
+		res.send({});
+	}
+	else {
+		var level = path.join(__dirname, '/levels/level' + req.params.num + '.csv');
+		var stream = fs.createReadStream(level);
+		stream.on('open', () => {
+			papa.parse(stream, {
+				complete: function(results, file) {
 					res.send(results.data);
-				}*/
-				res.send(results.data);
+				}
+			});
+		});
+	}
 
-
-			}
-		})
-	})
-
+	
 });
 
 package.compress(function() { 
